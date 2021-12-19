1ab799b155038481f1dca3b229f3ca453585577c
Raul Emilio Astete Acuña <raul.astete@encora.com>
2021-12-18 23:57:10 -0500

    Finish login screen with credentials validation

---
+++ app/src/main/java/com/rastete/loginvalidation/MainActivity.kt
@@ -0,0 +1,155 @@
+package com.rastete.loginvalidation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.rastete.loginvalidation.ui.theme.LoginValidationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LoginValidationTheme {
+                SignInScreen()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun SignInScreen() {
+    var username by remember {
+        mutableStateOf("")
+    }
+    var password by remember {
+        mutableStateOf("")
+    }
+    var isPasswordVisible by remember {
+        mutableStateOf(false)
+    }
+    val isFormValid by derivedStateOf {
+        username.isNotBlank() && password.length >= 7
+    }
+
+    val (focusRequester) = FocusRequester.createRefs()
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Scaffold(backgroundColor = MaterialTheme.colors.primary) {
+        Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Top) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_visa),
+                contentDescription = "App Logo",
+                modifier = Modifier
+                    .weight(1f)
+                    .size(200.dp),
+                colorFilter = ColorFilter.tint(Color.White)
+            )
+            Card(
+                Modifier
+                    .weight(2f)
+                    .padding(8.dp),
+                shape = RoundedCornerShape(32.dp)
+            ) {
+                Column(
+                    Modifier
+                        .fillMaxSize()
+                        .padding(32.dp)
+                ) {
+                    Text(text = "Welcome Back!", fontWeight = FontWeight.Bold, fontSize = 32.sp)
+                    Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+                        Spacer(modifier = Modifier.weight(1f))
+                        OutlinedTextField(
+                            modifier = Modifier.fillMaxWidth(),
+                            value = username,
+                            onValueChange = { username = it },
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text, imeAction = ImeAction.Next),
+                            keyboardActions = KeyboardActions(
+                                onNext = { focusRequester.requestFocus() }
+                            ),
+                            label = { Text(text = "Username") },
+                            singleLine = true,
+                            trailingIcon = {
+                                if (username.isNotBlank())
+                                    IconButton(onClick = { username = "" }) {
+                                        Icon(imageVector = Icons.Filled.Clear, contentDescription = "")
+                                    }
+                            }
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        OutlinedTextField(
+                            modifier = Modifier.fillMaxWidth().focusRequester(focusRequester),
+                            value = password,
+                            onValueChange = { password = it },
+                            label = { Text(text = "Password") },
+                            singleLine = true,
+                            keyboardActions = KeyboardActions(
+                                onDone = { keyboardController?.hide() }
+                            ),
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password, imeAction = ImeAction.Done),
+                            visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                            trailingIcon = {
+                                IconButton(onClick = { isPasswordVisible = !isPasswordVisible }) {
+                                    Icon(
+                                        imageVector = if (isPasswordVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff,
+                                        contentDescription = "Password Toggle"
+                                    )
+                                }
+                            }
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Button(
+                            onClick = {},
+                            enabled = isFormValid,
+                            modifier = Modifier.fillMaxWidth(),
+                            shape = RoundedCornerShape(16.dp)
+                        ) {
+                            Text(text = "Log In")
+                        }
+                        Spacer(modifier = Modifier.weight(1f))
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            TextButton(onClick = {}) {
+                                Text(text = "Sign Up")
+                            }
+                            TextButton(onClick = { }) {
+                                Text(text = "Forgot Password?", color = Color.Gray)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/rastete/loginvalidation/ui/theme/Color.kt
@@ -0,0 +1,5 @@
+package com.rastete.loginvalidation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val MainColor = Color(0xff6162F5)
